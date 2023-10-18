6778080d4d86509d7e60dbda94b7b4ec67961789
Saidul Arefin <114782285+Saidul-Arefin@users.noreply.github.com>
2023-10-19 00:34:21 +0600

    added dynamic brand cards

---
+++ src/Component/Page/Cards/BrandCard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const BrandCard = ({ brand }) => {
+  const { brandName, imageUrl } = brand;
+
+  return (
+    <div>
+      <div className="card  bg-base-100 shadow-xl image-full">
+        <figure>
+          <img src={imageUrl} alt={brandName} />
+        </figure>
+        <div className="card-body  flex-col justify-end">
+          <div className=" flex justify-center ">
+            <h2 className="card-title uppercase text-white  text-4xl">
+              {brandName}
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BrandCard;
--- src/Component/Page/Home.jsx
+++ src/Component/Page/Home.jsx
@@ -1,9 +1,18 @@
+import { useLoaderData } from "react-router-dom";
 import Banner from "../Header/Banner";
+import BrandCard from "./Cards/BrandCard";
 
 const Home = () => {
+  const brands = useLoaderData();
+  console.log(brands);
   return (
-    <div className="h-screen">
+    <div className="min-h-screen">
       <Banner></Banner>
+      <div className="container px-2 my-20 mx-auto grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {brands.map((brand) => (
+          <BrandCard key={brand.id} brand={brand}></BrandCard>
+        ))}
+      </div>
     </div>
   );
 };
